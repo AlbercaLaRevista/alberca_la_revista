d23ccbd222263aeec53f317673a335aa1b61f570
AlbercaLaRevista <gimr.carlosayonhdez@gmail.com>
2024-06-25 09:23:12 -0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,67 +2,55 @@
 
 if(!$_POST) exit;
 
-// Email address verification, do not edit.
+// Función para validar formato de correo electrónico
 function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+    return preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|...|zw)$/i", $email);
 }
 
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
-
-$form_name     = $_POST['form_name'];
-$email    = $_POST['email'];
-$phone   = $_POST['phone'];
-$no_of_persons   = $_POST['no_of_persons'];
+// Captura de datos del formulario
+$form_name = $_POST['form_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$no_of_persons = $_POST['no_of_persons'];
+$date_picker = $_POST['date-picker'];
+$time_picker = $_POST['time-picker'];
 $preferred_food = $_POST['preferred_food'];
 $occasion = $_POST['occasion'];
-$verify   = $_POST['verify'];
 
+// Validación de campos obligatorios
 if(trim($form_name) == '') {
-	echo '<div class="error_message">Attention! You must enter your name.</div>';
-	exit();
-}  else if(trim($email) == '') {
-	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
-	exit();
-} else if(!isEmail($email)) {
-	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
-	exit();
+    echo '<div class="error_message">Atención! Debes ingresar tu nombre.</div>';
+    exit();
+} elseif(trim($email) == '') {
+    echo '<div class="error_message">Atención! Por favor ingresa una dirección de correo electrónico válida.</div>';
+    exit();
+} elseif(!isEmail($email)) {
+    echo '<div class="error_message">Atención! Has ingresado una dirección de correo electrónico inválida, inténtalo de nuevo.</div>';
+    exit();
+} elseif(trim($date_picker) == '') {
+    echo '<div class="error_message">Atención! Por favor ingresa la fecha.</div>';
+    exit();
+} elseif(trim($time_picker) == '') {
+    echo '<div class="error_message">Atención! Por favor ingresa la hora.</div>';
+    exit();
 }
 
-if(trim($date-picker) == '') {
-	echo '<div class="error_message">Attention! Please enter your date.</div>';
-	exit();
-}
+// Configuración del correo electrónico
+$address = "gimr.carlosayonhdez@gmail.com"; // Dirección de correo a la que se enviará el formulario
 
-if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
-}
+$e_subject = 'Has sido contactado por ' . $form_name . '.';
 
+$e_body = "Has sido contactado por $form_name. Detalles de la reserva:" . PHP_EOL . PHP_EOL;
+$e_content = "Correo electrónico: $email" . PHP_EOL;
+$e_content .= "Teléfono: $phone" . PHP_EOL;
+$e_content .= "Número de personas: $no_of_persons" . PHP_EOL;
+$e_content .= "Fecha: $date_picker" . PHP_EOL;
+$e_content .= "Hora: $time_picker" . PHP_EOL;
+$e_content .= "Preferencia de comida: $preferred_food" . PHP_EOL;
+$e_content .= "Tipo de evento: $occasion" . PHP_EOL . PHP_EOL;
+$e_reply = "Puedes contactar a $form_name por correo electrónico, $email, o por teléfono $phone";
 
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-//$address = "example@themeforest.net";
-$address = "example@yourdomain.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
-$e_subject = 'You\'ve been contacted by ' . $form_name . '.';
-
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
-$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $first_name via email, $email or via phone $phone";
-
-$msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+$msg = wordwrap($e_body . $e_content . $e_reply, 70);
 
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
@@ -70,19 +58,17 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
+// Envío del correo electrónico
 if(mail($address, $e_subject, $msg, $headers)) {
-
-	// Email has sent successfully, echo a success page.
-
-	echo "<fieldset>";
-	echo "<div id='success_page'>";
-	echo "<h1>Email Sent Successfully.</h1>";
-	echo "<p>Thank you <strong>$form_name</strong>, your message has been submitted to us.</p>";
-	echo "</div>";
-	echo "</fieldset>";
-
+    // Éxito al enviar el correo electrónico
+    echo "<fieldset>";
+    echo "<div id='success_page'>";
+    echo "<h1>Correo enviado exitosamente.</h1>";
+    echo "<p>Gracias <strong>$form_name</strong>, tu mensaje ha sido enviado correctamente.</p>";
+    echo "</div>";
+    echo "</fieldset>";
 } else {
-
-	echo 'ERROR!';
-
+    // Error al enviar el correo electrónico
+    echo 'ERROR!';
 }
+?>
